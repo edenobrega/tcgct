2a989f1f3551217da58137f0e82eae1b1835a1df
Enrique <me@enriquedenobrega.com>
2023-03-14 20:16:20 +0000

    Create interface for table items

---
+++ tcgct/Data/Interface/ITableItem.cs
@@ -0,0 +1,9 @@
+﻿namespace tcgct.Data.Interface
+{
+    public interface ITableItem
+    {
+        string Display { get; set; }
+        string Visibility { get; set; }
+        bool Filtered { get; set; }
+    }
+}
