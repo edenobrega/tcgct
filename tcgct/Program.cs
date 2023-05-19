35e14df708dc3ffc281f0cc25a48c42606abf5ed
nobs <edenobrega@sky.com>
2023-05-19 21:46:58 +0100

    remove weatherforecast

--- tcgct/Program.cs
+++ tcgct/Program.cs
@@ -27,7 +27,6 @@ namespace tcgct
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-            builder.Services.AddSingleton<WeatherForecastService>();
             builder.Services.AddScoped<MTGService>();
 
             var app = builder.Build();
