0dc0e70536402a0c049fa887d3550015e3b985b4
nobs <edenobrega@sky.com>
2023-05-19 21:49:51 +0100

    Create MTGViewCard.cs

---
+++ tcgct/Data/MTG/MTGViewCard.cs
@@ -0,0 +1,14 @@
+﻿using tcgct.Data.Base;
+using tcgct_mtg.Models;
+
+namespace tcgct.Data.MTG
+{
+    public class MTGViewCard : TableItem
+    {
+        public MTGViewCard(Card Card)
+        {
+            this.Card = Card;
+        }
+        public Card Card { get; set; }
+    }
+}
