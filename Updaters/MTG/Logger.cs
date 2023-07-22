138321f0fdbb750859c16b6d06472e19c134418d
nobs <edenobrega@sky.com>
2023-07-22 18:54:59 +0100

    Data Updater imrpovements

---
+++ Updaters/MTG/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTG
+{
+	internal static class Logger
+	{
+        public static bool ShouldLog { get; set; }
+        public static void Log(string type, string message)
+		{
+            if (!ShouldLog)
+            {
+                return;
+            }
+            Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}:{DateTime.Now.Millisecond} | {type} | {message}");
+        }
+    }
+}
