4e227535f3492168afb779f4e1e522b9a66ebaae
nobs <edenobrega@sky.com>
2023-07-23 19:15:40 +0100

    Implement first unit tests

---
+++ tcgct-mtg-tests/Initialize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tcgct_mtg_tests
+{
+    [TestClass]
+	public class Initialize
+	{
+		public const string DB_NAME = "tcgct_unittest_db";
+		[AssemblyInitialize]
+		public static void TestsInitialize(TestContext testContext)
+		{
+			DatabaseBuilder.DatabaseBuilder.BuildTestDB(DB_NAME);
+		}
+
+		[AssemblyCleanup]
+		public static void TestsCleanup()
+		{
+			DatabaseBuilder.DatabaseBuilder.DropTestDB(DB_NAME);
+		}
+	}
+}
--- tcgct-mtg-tests/Rarity.cs
+++ tcgct-mtg-tests/Rarity.cs
@@ -1,11 +1,40 @@
+using tcgct_mtg;
+using tcgct_mtg.Services;
+
 namespace tcgct_mtg_tests
 {
 	[TestClass]
 	public class Rarity
 	{
-		[TestMethod]
-		public void TestMethod1()
+		internal static MTGService service;
+		[ClassInitialize]
+		public static void ClassInitialize(TestContext context)
 		{
+			configuration.ConfigureConnectionString($"Server=localhost\\SQLEXPRESS;Database={Initialize.DB_NAME};Trusted_Connection=True;");
+			service = new MTGService();
+		}
+
+		[TestMethod]
+		public void GetAllRarities()
+		{
+			var actual = service.GetRarities().ToList();
+			Assert.IsTrue(actual.Any(a => a.ID == 1 && a.Name == "Normal"));
+			Assert.IsTrue(actual.Any(a => a.ID == 2 && a.Name == "Uncommon"));
+		}
+
+		[TestMethod]
+		public void GetRarity()
+		{
+			var actual = service.GetRarity(1);
+			Assert.IsTrue(actual.ID == 1 && actual.Name == "Normal");
+		}
+
+		[TestMethod]
+		public void CreateRarity()
+		{
+			var id = service.CreateRarity("Rare");
+			var actual = service.GetRarity(id);
+			Assert.IsTrue(actual.ID == id && actual.Name == "Rare");
 		}
 	}
 }
