bff216457de27b3e5674ff9d8414b27a34960a96
nobs <edenobrega@sky.com>
2023-08-29 00:33:07 +0100

    Add identity

--- tcgct-mud/Program.cs
+++ tcgct-mud/Program.cs
@@ -1,6 +1,10 @@
 using MudBlazor.Services;
 using tcgct_mtg.Services;
 using tcgct_services_framework.MTG;
+using Microsoft.EntityFrameworkCore;
+using tcgct_mud.Data;
+using tcgct_mud.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace tcgct_mud
 {
@@ -9,13 +13,19 @@ namespace tcgct_mud
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("IdentityContextConnection") ?? throw new InvalidOperationException("Connection string 'IdentityContextConnection' not found.");
+
+            builder.Services.AddDbContext<IdentityContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<IdentityContext>();
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddMudServices();
-            //builder.Services.AddScoped<IMTGService, MTGSqlService>();
             builder.Services.AddScoped<IMTGService>(di => new MTGSqlService(builder.Configuration.GetSection("ConnectionStrings")["MainDB"]));
-            //builder.Configuration
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -34,6 +44,7 @@ namespace tcgct_mud
 
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
+            app.UseAuthentication();;
 
             app.Run();
         }
